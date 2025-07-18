2384c19d2817b04269d0dd7d33aa990a9083db48
lasercata <67599917+lasercata@users.noreply.github.com>
2025-07-18 23:50:47 +0200

    Changing query timeout from 5s to 20s

--- src/services/axios.ts
+++ src/services/axios.ts
@@ -3,7 +3,7 @@ import axios from 'axios';
 
 const api = axios.create({
   baseURL: import.meta.env.VITE_BASE_URL || 'http://localhost:3000/',
-  timeout: 5000,
+  timeout: 20000, // 20000ms = 20s
   headers: {
     'Content-Type': 'application/json',
   },
